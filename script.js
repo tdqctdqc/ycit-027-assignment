57ce2faac756efca564e8e245782248435e7cfe1
tdqctdqc <tynangerald@gmail.com>
2022-11-29 11:43:23 -0500

    Fixed 1st bug

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ function getCatImage() {
     fetch("https://api.thecatapi.com/v1/images/search")
         .then((response) => {
             // Resolve the promise we got from calling then() to the promise that we get from calling json()
-            response.json();
+            return response.json();
         })
         .then((data) => {
             console.log(data[0].url);
             document.getElementById("catImage").src = data[0].url;
         });
 }
+
+getCatImage();
